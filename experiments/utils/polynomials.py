20e61459a43b7b48b1b4b61d9862f9a26fdf9a6e
Rohan <59912533+RohanVKashyap@users.noreply.github.com>
2022-10-10 14:25:25 +0530

    Update polynomials.py

--- experiments/utils/polynomials.py
+++ experiments/utils/polynomials.py
@@ -1,12 +1,38 @@
 import tensorflow as tf
 
 
+def poly_Z2(x):
+    def inv1(a, b):
+        return a * b ** 2
+
+    a, b = tf.unstack(x, axis=1)
+    q1 = inv1(a, b) + inv1(b, a)
+    return q1
+
+def poly_Z4(x):
+    def inv1(a, b):
+        return a * b ** 2
+
+    a, b, c, d = tf.unstack(x, axis=1)
+    q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, a)
+    return q1
+
+
 def poly_Z5(x):
     def inv1(a, b):
         return a * b ** 2
 
     a, b, c, d, e = tf.unstack(x, axis=1)
     q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, e) + inv1(e, a)
+    return q1
+
+def poly_Z8(x):
+    def inv1(a, b):
+        return a * b ** 2
+
+    a, b, c, d, e, f , g , h = tf.unstack(x, axis=1)
+    q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, e) + inv1(e, f) \
+         + inv1(f, g) + inv1(g, h) + inv1(h, a)
     return q1
 
 
@@ -20,14 +46,6 @@ def poly_Z10(x):
     return q1
 
 
-def poly_Z5_Z10(x):
-    def inv1(a, b):
-        return a * b ** 2
-
-    a, b, c, d, e, f , g , h, i, j  = tf.unstack(x, axis=1)
-    q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, e) + inv1(e, a)
-    return q1
-
 
 def poly_Z16(x):
     def inv1(a, b):
@@ -37,6 +55,14 @@ def poly_Z16(x):
     q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, e) + inv1(e, f) + inv1(f, g) + inv1(g, h) \
          + inv1(h, i) + inv1(i, j) + inv1(j, k) + inv1(k, l)+ inv1(l, m)+ inv1(m, n) + inv1(n, o) \
          + inv1(o, p) + inv1(p, a)
+    return q1
+
+def poly_Z5_Z10(x):
+    def inv1(a, b):
+        return a * b ** 2
+
+    a, b, c, d, e, f , g , h, i, j  = tf.unstack(x, axis=1)
+    q1 = inv1(a, b) + inv1(b, c) + inv1(c, d) + inv1(d, e) + inv1(e, a)
     return q1
 
 
